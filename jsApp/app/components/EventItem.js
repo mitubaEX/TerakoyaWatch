a582381349f251e6368d18ff57553b073881fca0
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-03-02 05:22:17 +0900

    set layout

---
+++ jsApp/app/components/EventItem.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { ListItem, Text, Body, Button, Icon, Right, Left } from 'native-base';
+
+export default class EventItem extends Component {
+  render() {
+    return (
+      <ListItem icon>
+        <Left>
+          <Text>2/28</Text>
+        </Left>
+        <Body>
+          <Text>{this.props.item}</Text>
+        </Body>
+        <Right>
+          <Button transparent onPress={() => console.log('hello')}>
+            <Icon name="arrow-forward" />
+          </Button>
+        </Right>
+      </ListItem>
+    );
+  }
+}
--- jsApp/app/components/Main.js
+++ jsApp/app/components/Main.js
@@ -3,51 +3,43 @@ import React, { Component } from 'react';
 import {
   View,
   StyleSheet,
-  Platform
+  Platform,
+  FlatList,
+  Text
 } from 'react-native';
 
 import VisibleTodoList from '../containers/VisibleTodoList'
 import AddTodo from '../containers/AddTodo'
 
-import {Calendar, CalendarList} from 'react-native-calendars'
+import { Container, Header, Content, List, ListItem, Body, Title, Separator } from 'native-base';
+
+import EventList from './EventList'
+import MyCalendar from './Calendar'
 
 class MainContainer extends Component {
   render() {
     // この部分はビューをレンダーです。
     return (
-      <View style={styles.container}>
-        <Calendar
-          onDayPress={(day) => {console.log('selected day', day)}}
-          monthFormat={'yyyy MM'}
-          onMonthChange={(month) => {console.log('month changed', month)}}
-          hideExtraDays={true}
-          hideArrows={false}
-          firstDay={1}
-          showWeekNumbers={false}
-          style={styles.calendar}
-        />
-      </View>
+      <Container>
+        <Header >
+          <Body>
+            <Title>Header</Title>
+          </Body>
+        </Header>
+        <Content>
+          <Separator bordered>
+            <Text>New Event</Text>
+          </Separator>
+          <EventList />
+          <Separator bordered>
+            <Text>Calendar</Text>
+          </Separator>
+          <MyCalendar />
+        </Content>
+      </Container>
     );
   }
 }
-
-// ビューのスタイル修正
-const styles = StyleSheet.create({
-  calendar: {
-    borderTopWidth: 1,
-    paddingTop: 5,
-    borderBottomWidth: 1,
-    borderColor: '#eee',
-    height: 350
-  },
-  container: {
-    marginTop: Platform.OS === "ios" ? 20 : 0,
-    flex: 1,
-    justifyContent: "space-between",
-    flexDirection: "column",
-    backgroundColor: "#1D9FF2"
-  }
-});
 
 // このContainerを利用できるためエクスポートします
 export default MainContainer;
