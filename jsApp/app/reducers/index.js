369e0ed6213be9f524f4369825f2460e865511cd
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-03-02 14:46:10 +0900

    余分なファイルを削除

--- jsApp/app/reducers/index.js
+++ jsApp/app/reducers/index.js
@@ -1,14 +1,10 @@
 import { combineReducers } from 'redux'
-import todos from './todos'
-import visibilityFilter from './visibilityFilter'
 import events from './EventReducers'
 
 /**
  *  * Reducerコンパイル
  *   */
 const todoApp = combineReducers({
-  todos,
-  visibilityFilter,
   events
 })
 
