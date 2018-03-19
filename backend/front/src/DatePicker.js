d266ceab2714accc243751d265aee1a5cfba760d
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-03-19 23:49:24 +0900

    add simple management page

---
+++ backend/front/src/DatePicker.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+import {firebaseDb} from './firebase/';
+
+import 'react-datepicker/dist/react-datepicker.css';
+
+// CSS Modules, react-datepicker-cssmodules.css
+// import 'react-datepicker/dist/react-datepicker-cssmodules.css';
+
+export default class MyDatePicker extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      value: '',
+      body: '',
+      startDate: moment(),
+      dateString: moment().format('YYYY-MM-DD'),
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleFormChange = this.handleFormChange.bind(this);
+    this.handleBodyChange = this.handleBodyChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(date) {
+    this.setState({
+      startDate: date,
+      dateString: date.format('YYYY-MM-DD'),
+    });
+  }
+
+  handleFormChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleBodyChange(event) {
+    this.setState({body: event.target.value});
+  }
+
+  handleSubmit(event) {
+    const sendData = {title: this.state.value, body: this.state.body, date: this.state.dateString, id: this.props.data.length + 1}
+    firebaseDb.ref('data').set(this.props.data.concat([sendData]))
+    alert('A name was submitted: ' + this.state.value + this.state.body + this.state.dateString);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <DatePicker
+          selected={this.state.startDate}
+          onChange={this.handleChange}
+        />
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input type="text" value={this.state.value} onChange={this.handleFormChange} />
+          </label>
+          <label>
+            Body:
+            <input type="text" value={this.state.body} onChange={this.handleBodyChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
+}
---
+++ backend/front/src/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBv8T2IvJueP03nxLYjez9SIiAl4fe1LV0",
+  authDomain: "terakoyawatch.firebaseapp.com",
+  databaseURL: "https://terakoyawatch.firebaseio.com",
+  projectId: "terakoyawatch",
+  storageBucket: "terakoyawatch.appspot.com",
+  messagingSenderId: "495210227972"
+};
