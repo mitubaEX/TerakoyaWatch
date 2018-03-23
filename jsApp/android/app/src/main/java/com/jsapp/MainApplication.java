c0775705210d62ac7082489fcbb7653f6477e66e
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-03-24 02:39:32 +0900

    add ios configuration

--- jsApp/android/app/src/main/java/com/jsapp/MainApplication.java
+++ jsApp/android/app/src/main/java/com/jsapp/MainApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -26,6 +28,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
+            new ReactNativePushNotificationPackage(),
             new VectorIconsPackage(),
             new VectorIconsPackage(),
             new VectorIconsPackage()
